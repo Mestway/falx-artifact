58ac0f78a2059271931b4b385a870833c7e7705a
Chenglong Wang <mestway@gmail.com>
2019-10-21 15:50:02 -0700

    ok

--- init_docker.sh
+++ init_docker.sh
@@ -9,5 +9,9 @@ if [[ "$(docker images -q $IMG_NAME 2> /dev/null)" == "" ]]; then
     docker build -t $IMG_NAME .
 fi
 
-echo "[0] starting container with name $NAME ..."
+echo "[0] starting container with name $CONTAINER_NAME ..."
+if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+    echo "docker $CONTAINER_NAME already exist, remove it now"
+    docker rm $CONTAINER_NAME
+fi
 docker run -v $(pwd)/:$DIR -w $DIR --name=$CONTAINER_NAME -it $IMG_NAME bash
